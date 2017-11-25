4d5d85e5e6c5f2833fce5471a772fb9b361993df
raj <isujan@yahoo.com>
2017-11-25 12:01:34 -0500

    Changes for compiling framework and frameworkt tests on .net core 2.

--- src/NUnitFramework/tests/Syntax/TestCompiler.cs
+++ src/NUnitFramework/tests/Syntax/TestCompiler.cs
@@ -3,7 +3,7 @@
 // This is free software licensed under the NUnit license. You may
 // obtain a copy of the license at http://nunit.org.
 // ****************************************************************
-
+#if !NETCOREAPP2_0
 using System;
 using System.CodeDom.Compiler;
 using System.IO;
@@ -55,3 +55,4 @@ namespace NUnit.Framework.Syntax
         }
     }
 }
+#endif
